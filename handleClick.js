f35468b37aa83578a51222469025ff2c999a7bc9
Alan Puglisi <alan.c.puglisi@gmail.com>
2020-09-25 15:11:19 -0500

    added comments and fixed formatting

--- handleClick.js
+++ handleClick.js
@@ -1,6 +1,9 @@
+// select h1 to display emoji
 const emoji = document.querySelector("h1");
 
 document.getElementById("buttons").addEventListener("click", function(e) {
+	// set mood in store to match clicked button
 	store.dispatch({ type: e.target.id.toUpperCase() });
+	// display emoji in HTML
 	emoji.innerText = store.getState().mood;
 });
